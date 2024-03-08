3993dce5be30ed8b0b51f20c0a7294f4a40b1cd1
lordeber15 <eberpalomino@hotmail.com>
2024-03-07 20:11:40 -0500

    cambios pequeños para depliegue

--- src/database/database.js
+++ src/database/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 // const { URL_BD } = process.env;
-
+const { DB_DEPLOY } = process.env;
 const sequelize = new Sequelize(
-  "postgres://koyeb-adm:mE2bsjR8SfVJ@ep-purple-rain-a48f5enm.us-east-1.pg.koyeb.app/koyebdb"
+  DB_DEPLOY
   /* "postgres://postgres:123456@localhost:5432/qoriapp"*/
 );
 
