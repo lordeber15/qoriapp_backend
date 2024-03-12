713b67549be796a9b4ab4e808619cbea56e9126b
lordeber15 <eberpalomino@hotmail.com>
2024-03-11 22:14:37 -0500

    back actualizado

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const { DB_DEPLOY } = process.env;
 console.log(DB_DEPLOY);
 const sequelize = new Sequelize(
- /* "postgres://qoriapp_user:rXle6FdEvdFUNTM4yP6UeHxHRU90q2A8@dpg-cnl5r7gl6cac73edb7cg-a.oregon-postgres.render.com/qoriapp",
+  "postgres://qoriapp_user:rXle6FdEvdFUNTM4yP6UeHxHRU90q2A8@dpg-cnl5r7gl6cac73edb7cg-a.oregon-postgres.render.com/qoriapp",
   {
     dialect: "postgres",
     dialectOptions: {
@@ -11,9 +11,9 @@ const sequelize = new Sequelize(
         rejectUnauthorized: false, // Esto es opcional, dependiendo de la configuración de tu servidor PostgreSQL
       },
     },
-  }*/
+  }
 
-  "postgres://postgres:123456@localhost:5432/qoriapp"
+  /*"postgres://postgres:123456@localhost:5432/qoriapp"*/
 );
 
 module.exports = sequelize;
